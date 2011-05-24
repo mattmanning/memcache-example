45d1f2d11b2580b025bc289236ea63a685afa03a
Matt Manning <matt.manning@gmail.com>
2011-05-24 16:16:15 -0700

    Bundle and configure dalli

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ MemecacheExample::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.cache_store = :dalli_store
 end
