7f9a532a6409abae047fc988fb034bd7d9dbc00c
Matt Manning <matt.manning@gmail.com>
2011-05-24 16:41:55 -0700

    Create test controller and views for storing and fetching values.

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,17 @@
+class TestController < ApplicationController
+  def index
+    20.times do |i|
+      Rails.cache.write i, random_string(10)
+    end
+
+    @pairs = (1..20).inject([]) do |pairs, key|
+      pairs << [key, Rails.cache.read(key)]
+    end
+  end
+
+  private
+
+  def random_string(length)
+    (1..length).inject('') {|str, i| str += (65 + rand(26)).chr}
+  end
+end
