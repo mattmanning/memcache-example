87b4ab6c69e671a55083d49abea9530da7eee537
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-11-23 10:43:04 -0800

    Use the heroku environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ MemcacheExample::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.cache_store = :dalli_store
+  config.cache_store = :dalli_store,  ENV["MEMCACHE_SERVERS"],  
 end
