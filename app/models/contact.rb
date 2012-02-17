21d56cfb7f79219d087867003cb34d2744c1191d
Matt Manning <matt.manning@gmail.com>
2012-02-17 00:46:09 -0500

    Add cache expiration

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,12 @@
 class Contact < ActiveRecord::Base
+  after_save    :expire_contact_all_cache
+  after_destroy :expire_contact_all_cache
+
   def self.all_cached
     Rails.cache.fetch('Contact.all') { all }
+  end
+
+  def expire_contact_all_cache
+    Rails.cache.delete('Contact.all')
   end
 end
