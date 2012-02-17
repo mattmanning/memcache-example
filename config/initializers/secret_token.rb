df00848e685e023a1ef37f13c487780ed49a2def
Matt Manning <matt.manning@gmail.com>
2012-02-16 23:56:24 -0500

    Rebuild for rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemcacheExample::Application.config.secret_token = '290cf012aca7a2774c58329cb5454b02efd277c9bb37abc07dd29111028473a67b54452088712e7279d56e51ff9f17b573cf06355e96fa974aecc00d6826815d'
